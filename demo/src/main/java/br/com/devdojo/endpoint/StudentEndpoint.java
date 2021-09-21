d237358ce3722ce8d7ee96a4958d774e60f9e323
jeanAt2 <jean0king@gmail.com>
2021-09-20 23:51:46 -0300

    Primeira parte de atenticação de user. Amanhão continuamos do notbook mesmo
    @jeanAt2

--- demo/src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
+++ demo/src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -49,6 +50,7 @@ public class StudentEndpoint {
     }
 
     @DeleteMapping(path = "/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         verifyIfStudentExists(id);
         studentDao.deleteById(id);
